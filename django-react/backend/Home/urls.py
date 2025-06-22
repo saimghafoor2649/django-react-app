fa3fcda68a6e0e7e2c33b47c0f1c768e6a519f74
saimghafoor2649 <saimghafoor2649@gmail.com>
2025-06-22 21:53:04 +0500

    .git

--- django-react/backend/Home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import views
-from django.urls import path,include
-urlpatterns = [
-   path("Post/", views.PostListCreate.as_view(), name="post-list"),
-   path("posts/delete/<int:pk>/", views.PostDelete.as_view(), name="delete-post"),
-]
